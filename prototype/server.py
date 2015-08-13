6d9633b63c70628ddfe8ff3db9c5c3970f067111
Jeanderson Barros Candido <jeandersonbc@gmail.com>
2015-08-13 01:13:51 -0300

    Implemented endpoint to consume the XML resouce

--- prototype/server.py
+++ prototype/server.py
@@ -1,9 +1,17 @@
-from flask import Flask
+import urllib2
+from flask import Flask, Response
+
 app = Flask(__name__)
+
+@app.route("/clientes")
+def clients():
+    xmlUrl = "file:///vagrant/prototype/resources/clientes.xml"
+    xmlResouce = urllib2.urlopen(xmlUrl)
+    return Response(xmlResouce, mimetype="text/xml")
 
 @app.route("/")
 def index():
-    return "Hello Flask!"
+    return Response("Hello Flask!", mimetype="text/html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
